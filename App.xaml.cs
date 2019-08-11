af5a9438ffc4afdf68d4bd9644512071c80bd18c
jyxjjj <773933146@qq.com>
2019-08-12 04:49:08 +0800

    Moved

--- App.xaml.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Windows;
-
-namespace LOL_TFT
-{
-    public partial class App : Application
-    {
-    }
-}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LOL-TFT
- 英雄联盟云顶之弈助手
+英雄联盟云顶之弈助手
+
+#已迁移
+https://github.com/jyxjjj/CSharp-Utils
